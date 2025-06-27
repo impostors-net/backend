2507009989b951615c65e41654194e4723bb610f
pauljako <paulifey18@proton.me>
2025-06-28 00:11:20 +0200

    switched to connexion

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import flask
+import connexion
 
 DEFAULT_HOST = "0.0.0.0"
 DEFAULT_PORT = 4789
@@ -6,9 +6,12 @@ DEFAULT_PORT = 4789
 host = DEFAULT_HOST
 port = DEFAULT_PORT
 
-app = flask.Flask(__name__)
-
+app = connexion.App(__name__)
+app.add_api("./openapi.yaml")
 
 
 def run():
-    app.run(host, port)
+    app.run(host=host, port=port)
+
+
+if __name__ == "__main__": run()
