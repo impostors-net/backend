afcf0664ab57101cb28a3131a5a0c6ef17e1d027
pauljako <paulifey18@proton.me>
2025-06-28 20:04:22 +0200

    fixed

--- api/auth.py
+++ api/auth.py
@@ -2,20 +2,20 @@ import hashlib
 
 from database import User, DatabaseManager
 
-def signup(display_name: str, handle: str, password_unhash: str):
-    password_bytes = password_unhash.encode('utf-8')
+def signup(display_name: str, handle: str, password_hash: str):
+    password_bytes = password_hash.encode('utf-8')
 
     # Use SHA-256 hash function to create a hash object
     hash_object = hashlib.sha256(password_bytes)
 
     # Get the hexadecimal representation of the hash
-    password_hash = hash_object.hexdigest()
+    actual_password_hash = hash_object.hexdigest()
 
     manager = DatabaseManager()
     if User.get_by_handle(handle, manager):
         return 409
     else:
-        user = User(display_name, handle, password_hash, manager)
+        user = User(display_name, handle, actual_password_hash, manager)
         return {
             "uuid": user.id,
             "displayName": user.display_name,
