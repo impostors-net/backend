28a50df636c71f2446aaec9ed871e3f0927aa9d1
pauljako <paulifey18@proton.me>
2025-06-28 18:58:06 +0200

    yay

---
+++ api/cors.py
@@ -0,0 +1,42 @@
+from flask import make_response
+
+def post_next_options():
+    """Handle CORS preflight for /post/next"""
+    return _cors_response()
+
+def post_uuid_options(uuid):
+    """Handle CORS preflight for /post/{uuid}"""
+    return _cors_response()
+
+def post_options():
+    """Handle CORS preflight for /post"""
+    return _cors_response()
+
+def user_uuid_options(uuid):
+    """Handle CORS preflight for /user/{uuid}"""
+    return _cors_response()
+
+def post_comment_options(uuid):
+    """Handle CORS preflight for /post/{uuid}/comment"""
+    return _cors_response()
+
+def comment_uuid_options(uuid):
+    """Handle CORS preflight for /comment/{uuid}"""
+    return _cors_response()
+
+def comment_vote_options(uuid):
+    """Handle CORS preflight for /comment/{uuid}/vote"""
+    return _cors_response()
+
+def auth_signup_options():
+    """Handle CORS preflight for /auth/signup"""
+    return _cors_response()
+
+def _cors_response():
+    """Helper function to create CORS response"""
+    response = make_response()
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization,displayName,handle,passwordHash'
+    response.headers['Access-Control-Max-Age'] = '86400'
+    return response
